96ea75a7788be961d4abe77c39e52892ed1b9461
lightx0x <kingcairo4000@gmail.com>
2026-06-02 08:05:06 +0100

    feat(options): enable relative line numbers

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -7,3 +7,11 @@ vim.o.smartcase = true -- Smart case (default: false)
 vim.o.linebreak = true -- Companion to wrap, don't split words (default: false)
 vim.o.hlsearch = false -- Set highlight on search (default: true)
 vim.o.clipboard = "unnamedplus" -- Sync clipboard between OS and Neovim. (default: '')
+-- keep more context on screen while scrolling
+vim.opt.scrolloff = 2
+-- never show me line breaks if they're not there
+vim.opt.wrap = false
+-- always draw sign column. prevents buffer moving when adding/deleting sign
+vim.opt.signcolumn = "yes"
+-- sweet sweet relative line numbers
+vim.opt.relativenumber = true
