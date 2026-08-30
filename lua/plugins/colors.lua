88dff38b6d7aa8e283dcf652676e9b1240a96dff
lightx0x <kingcairo4000@gmail.com>
2026-08-30 21:49:45 +0100

    Update config

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -5,24 +5,16 @@ end
 
 return {
 	{
-		"rebelot/kanagawa.nvim",
-		lazy = false, -- Load immediately
-		priority = 1000, -- Load first
+		"sainnhe/gruvbox-material",
+		lazy = false,
+		priority = 1000,
 		config = function()
-			require("kanagawa").setup({
-				transparent = true, -- Set to true if you want full transparency
-				theme = "dragon", -- Options: "wave", "dragon", "lotus"
-				compile = false,
-				background = {
-					dark = "dragon",
-					light = "lotus",
-				},
-			})
+			vim.g.gruvbox_material_background = "hard"
+			vim.g.gruvbox_material_transparent_background = 1
+			vim.g.gruvbox_material_foreground = "material"
 
-			-- vim.cmd.colorscheme("kanagawa")
-			-- vim.cmd.colorscheme("kanagawa-wave") -- default rich theme
-			vim.cmd.colorscheme("kanagawa-dragon") -- darker, more aggressive
-			-- vim.cmd.colorscheme("kanagawa-lotus") -- light theme
+			vim.o.background = "dark"
+			vim.cmd.colorscheme("gruvbox-material")
 			enable_transparency()
 		end,
 	},
@@ -33,7 +25,7 @@ return {
 			"nvim-tree/nvim-web-devicons",
 		},
 		opts = {
-			theme = "kanagawa", -- Changed from tokyonight
+			theme = "gruvbox-material",
 		},
 	},
 }
